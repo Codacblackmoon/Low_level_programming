93b70d26574185cd06051d4dc3f7d8cf3a49fa55
blackmoom2 <gideonlenge96@gmail.com>
2022-10-16 10:30:26 -0700

    num2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,14 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the alphabet in lowercase.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -11,7 +16,8 @@ int main(void)
 
 	for (word == 'a'; word <= 'z'; word++)
 		putchar(word);
+		
+	putchar('\n');
 
-			putchar('\n');
 	return (0);
 }

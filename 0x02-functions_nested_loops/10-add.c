815cd5e744078fadd5269d7ac9d763420bdde526
blackmoom2 <gideonlenge96@gmail.com>
2022-10-19 14:07:32 -0700

    num11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result.
+ * @n: first num
+ * @x: seconde num
+ * Return:n+x
+ */
+int add(int n, int x)
+{
+	return (n + m);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints the 9 times table, starting with 0
+ * rone = row, col = column, d = digits of current result
+ * Return: time table
+ * add extra space past single digit
+ */
+void times_table(void)
+{
+	int rone, col d;
+
+	for (rone = 0; rone <= 9; rone++;
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (col = 0; col <= 9; col++;)
+		{
+			d = (rone * col);
+			if ((d / 10) + '0')
+			{
+				_putchar((d % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (cone < 9)
+			{
+				_putchar(',');
+				_putchat(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}

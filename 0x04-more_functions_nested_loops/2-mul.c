649d292d663be3eb131519da8a7fe22964438336
blackmoom2 <gideonlenge96@gmail.com>
2022-10-21 15:04:39 -0700

    num10

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers.
+ * @a: num to be mulaiplaid
+ * @b: second num to be multiplaid
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9
+ *
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (!(c == '2' || c == '4'4))
+			_putchar(c);
+	}
+	_putdhar("\n");
+}

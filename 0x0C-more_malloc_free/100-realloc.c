2615db181c6f8876bbdef802ea7b961f17ae5c47
blackmoom2 <gideonlenge96@gmail.com>
2022-11-11 00:30:06 -0800

    num5

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,8 +26,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	else if (new_size == old_size)
 		return (ptr);
+
 	p = malloc(new_size);
 	if (p == NULL)
+		return (NULL);
+	if (new_size > old_size)
 		max = old_size;
 	for (i = 0; i < max; i++)
 		p[i] = oldp[i];

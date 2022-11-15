7336564880e05f465254e333cdfdec551e52d7e5
blackmoom2 <gideonlenge96@gmail.com>
2022-11-15 00:40:57 -0800

    0x0E

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdlib>
+#include <stdio.h>
+
+/**
+ * print_dog - main functione
+ * @d: input
+ * Return: 0
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		printf("Name: %s\n", d->name);
+	}
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+}

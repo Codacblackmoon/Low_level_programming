fec66786c3938f73bb9443f6920f8ffd6faf5696
blackmoom2 <gideonlenge96@gmail.com>
2022-11-16 12:27:20 -0800

    0x0f

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointer.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: is pointer
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	else
+	{
+		f(name);
+	}
+}

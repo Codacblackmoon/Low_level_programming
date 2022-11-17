cf70ab4a3f985188ebade455f8fd4f36f0d94302
blackmoom2 <gideonlenge96@gmail.com>
2022-11-17 05:06:17 -0800

    num3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -31,12 +31,15 @@ void print_all(const char * const format, ...)
 			case 'c':
 				printf("%c", va_arg(valist, int)), c = 1;
 				break;
+			case 'i':
+				printf("%d", va_arg(valist, int)), c = 1;
+				break;
 			case 'f':
 				printf("%f", va_arg(valist, double)), c = 1;
 				break;
-				case 's':
+			case 's':
 				str = va_arg(valist, char *), c = 1;
-				if (!str)
+				if (!str)	
 				{
 					printf("(nil)");
 					break;

f80881ef22017e44fe135aa407e576f6fb64ec3a
blackmoom2 <gideonlenge96@gmail.com>
2022-11-17 04:52:00 -0800

    num3

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VARIADIV_FUNCTIONS_H_
+#define VARIADIV_FUNCTIONS_H_
+
+#include <stdarg.h>
+#include <stdio.h>
+
+void print_all(const char * const format, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+#endif
